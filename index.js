cfc4cae8cc03e81733c56f1528987275e0e1d00d
unknown <ksaisudha2002@gmail.com>
2025-05-28 12:45:21 +0530

    new changes

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
- 
+ require("dotenv").config();
+const express = require("express");
+
+const app = express();
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
